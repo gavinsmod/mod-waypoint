6c764d87bd14b9d85fda29fcd9b14b1eec781066
Gavin <gavinpease@gmail.com>
2023-07-21 12:25:19 -0600

    Remove logging, and optionally removed mixins file if required

--- src/main/java/com/peasenet/mod/WaypointMod.java
+++ src/main/java/com/peasenet/mod/WaypointMod.java
@@ -9,14 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WaypointMod implements ModInitializer {
-    public static Logger LOGGER = LoggerFactory.getLogger("gavinsmod");
 
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
-        LOGGER.info("Hello Fabric world!");
         Settings.addConfig(new WaypointConfig());
         GavinsMod.addMod(new ModWaypoint());
     }
